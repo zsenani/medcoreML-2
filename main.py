ad05289ad038adb5a95ca14a555277d1fd6b2782
zsenani <44804489+zsenani@users.noreply.github.com>
2023-01-23 20:55:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from flask import Flask
+import json
+
 app = Flask(__name__)
 
+tasks = [
+    {
+        'id': 1,
+        'name': "task1",
+        "description": "This is task 1"
+    },
+    {
+        "id": 2,
+        "name": "task2",
+        "description": "This is task 2"
+    },
+    {
+        "id": 3,
+        "name": "task3",
+        "description": "This is task 3"
+    }
+]
+
+tasksJSON = json.dumps(tasks)
+
+
 @app.route('/')
-def hello():
-    sum = 1 + 1
-    return jsonify({'sum' : sum})
+def home():
+    return "App Works!!!"
+
+
+@app.route('/api/tasks')
+def tasks():
+    return tasksJSON
