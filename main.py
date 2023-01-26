7da0e89826909cae0027b5650d79bb3e9f697f32
CadyALsuhibani <93075937+CadyALsuhibani@users.noreply.github.com>
2023-01-26 22:46:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, jsonify
-from urllib.request import urlopen
+from flask import Flask, jsonify, request
 import pandas as pd
 from scipy.spatial.distance import squareform
 from scipy.spatial.distance import pdist, jaccard
@@ -20,28 +19,35 @@ def tasks():
         vector = request_data['vector'] #assigning it to name
         dataset = pd.read_csv('https://github.com/zsenani/2022-GP-10/blob/37911ec695b28ee7b9d8d10e5e184d83190c98ea/MedCoreDataset.csv?raw=true')
         df = pd.DataFrame(dataset)
+       
         df_new = df.iloc[:, 1:405]
         df_new.loc[len(df_new)] = vector
+        
         #[0,	0,	0,	0,	0,	1,	0,	1,	0,	0,	0,	0,	0,	0,	1,	1,	0,	1,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0, 0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	1,	0,	0,	0,	0,	0,	0,	1,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0,  0,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0	,0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0	,0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	1,	1,	1,	1,	1,	0,	0,	0,	0,	0,	0]
         # Calculate similarity
 
         res = pdist(df_new, 'jaccard')
         squareform(res)
+        print(res)
         distance =  1 - pd.DataFrame(squareform(res), index=df_new.index, columns= df_new.index)
+        print(distance)
         last_row_df = distance.iloc[-1:]
         last_row_json = last_row_df.to_json(orient ='records')
+        print(last_row_json)
         jsondict = json.loads(last_row_json)
         first = list(jsondict)[0]
         s = pd.Series(first)
+        print('ssss')
+        print(s)
         rows = len(df.axes[0])
 
         arr = {}
         index = 0
         for i in s:
-            if(i >= 0.3 and index != rows):
+            if(i >= 0.01 and index != rows):
                 arr[index] = i
             index +=1
-
+        print(arr)
         arr1 = {}
         for i in arr:
             aa = df.iloc[i:i+1, 0:1]
@@ -53,8 +59,8 @@ def tasks():
         sorted_arr1 = sorted(arr1.items(), key=lambda x:x[1], reverse=True)
 
 
-        tasksJSON = json.dumps(tasks)
-        #tasksJSON = jsonify({'name' : sorted_arr1})
+        print(arr1)
+        print(sorted_arr1)
         return jsonify({'vector' : sorted_arr1})
     else: 
         return 'ELSE'
