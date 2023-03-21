bd8598aafc9e1bd48daa186902008fc9b37d9e04
zsenani <44804489+zsenani@users.noreply.github.com>
2023-03-21 20:20:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def tasks():
         #request_data = json.loads(request_data.decode('utf-8')) #converting it from json to key value pair
         #vector = request_data['vector'] #assigning it to name
         vector = [1,	0,	0,	0,	0,	0,	0,	0,	0,	0, 0,	0,	0,	0,	1,	1,	0,	1,	0,	0,1,	0,	0,	0,	0,	1,	0,	1,	0,	0,0,	0,	0,	0,	0,	1,	0,	1,	0,	0,0,0,	0,	0,	0,	1,	0,	1,	0,	0,0,	0,	0,	0,	0,	1,	0,	1,	0,	0,0,	0,	0,	0,	0,	1,	0,	1,	0,	0,0,	0,	0,	0,	0,	1,	0,	1,	0,	0,0,	0,	0,	0,	0,	1,	0,	1,	0,	0,0,	0,	0,	0,	0,	1,	0,	1,	0,	0,0,	0,	0,	0,	0,	1,	0,	1,	0,	0,0,	0,	0,	0,	0,	1,	0,	1,	0,	0,0,	0,	0,	0,	0,	1,	0,	1,	0,	0,0,0]
-        dt = joblib.load('finalized_model.sav')
+        dt = joblib.load('https://github.com/zsenani/medcoreML-2/blob/bd6b027876bd1b0de0e25b728cf77dbaad9e40eb/finalized_model.sav')
         pca = joblib.load('pca.sav')
         vector = pca.transform([vector])
         predict = dt.predict(vector)
