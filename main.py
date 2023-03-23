378f9d9240c04529d1c11fad5e711d961699f5ab
zsenani <44804489+zsenani@users.noreply.github.com>
2023-03-23 22:16:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ def tasks():
         #request_data = json.loads(request_data.decode('utf-8')) #converting it from json to key value pair
         #vector = request_data['vector'] #assigning it to name
         vector = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,0,0,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0]
-        pca_reload = pk.load(open("pca.pkl",'rb'))
-        dt_reload = pk.load(open("dt.pkl",'rb'))
-        vector = pca_reload.transform([vector])
-        predict = dt_reload.predict(vector)
-        #predict = dt.predict_proba(vector)
+        with open("pca.pkl", 'rb') as pca:
+            pca = pk.load(pca)
+        with open("dt.pkl", 'rb') as dt:
+            dt = pk.load(dt)
+        vector = pca.transform([vector])
+        predict = dt.predict(vector)
         return jsonify({'vector' : predict[0]})
     else: 
         return 'ELSE'
