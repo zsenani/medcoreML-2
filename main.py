6e5b70298e1c22c282d2dbe196f1783c2272cbd1
zsenani <44804489+zsenani@users.noreply.github.com>
2023-03-23 23:46:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def home():
 
 @app.route('/search', methods = ['GET', 'POST'])
 def tasks():
-    if(request.method == 'GET'):
-        #request_data = request.data #getting the response data
-        #request_data = json.loads(request_data.decode('utf-8')) #converting it from json to key value pair
-        #vector = request_data['vector'] #assigning it to name
-        vector = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,0,0,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0]
+    if(request.method == 'POST'):
+        request_data = request.data #getting the response data
+        request_data = json.loads(request_data.decode('utf-8')) #converting it from json to key value pair
+        vector = request_data['vector'] #assigning it to vector
+        #vector = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,0,0,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0]
         with open("pca.pkl", 'rb') as pca:
             pca = pk.load(pca)
         with open("dt.pkl", 'rb') as dt:
