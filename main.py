0327479be0d9e118842054b509054d37e586b63a
zsenani <44804489+zsenani@users.noreply.github.com>
2023-03-25 01:24:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import json
 import pickle as pk
+from collections import Counter
 
 app = Flask(__name__)
 
@@ -12,6 +13,7 @@ def home():
 @app.route('/search', methods = ['GET', 'POST'])
 def tasks():
     if(request.method == 'POST'):
+        dataset = pd.read_csv('https://github.com/zsenani/medcoreML-2/blob/d9f5a98d70510682258f1e23bc3663c86a19455d/OO.csv?raw=true')
         request_data = request.data #getting the response data
         request_data = json.loads(request_data.decode('utf-8')) #converting it from json to key value pair
         vector = request_data['vector'] #assigning it to vector
@@ -22,6 +24,20 @@ def tasks():
             dt = pk.load(dt)
         vector = pca.transform([vector])
         predict = dt.predict(vector)
-        return jsonify({'vector' : predict[0]})
+        
+        #Get physID
+        df_physID = df.query('prognosis == @predict[0]').iloc[:, 132:134]
+        getPhys = df_physID.sample(n = 3)
+        getPhys = getPhys.physID.drop_duplicates()
+        
+        #Symptoms pie chart
+        df_Symptoms = df.query('prognosis == @predict[0]').iloc[:,0:132]
+        symptoms = list(df_Symptoms.columns)
+        symptoms_dic = dict()
+        for i in symptoms:
+            counts = Counter(df_Symptoms[i])
+            if(counts[1] != 0):
+                symptoms_dic[i] = counts[1]
+        return jsonify({'vector' : predict[0], 'physID' : list(getPhys), 'symptomsDic' : symptoms_dic})
     else: 
         return 'ELSE'
