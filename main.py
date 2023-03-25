b3213e9393239f3e740a3a3aba89f36e13c9a2b6
zsenani <44804489+zsenani@users.noreply.github.com>
2023-03-26 01:15:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ def home():
 @app.route('/search', methods = ['GET', 'POST'])
 def tasks():
     if(request.method == 'POST'):
-        dataset = pd.read_csv('https://github.com/zsenani/medcoreML-2/blob/d9f5a98d70510682258f1e23bc3663c86a19455d/OO.csv?raw=true')
-        df = pd.DataFrame(dataset)
         request_data = request.data #getting the response data
         request_data = json.loads(request_data.decode('utf-8')) #converting it from json to key value pair
         vector = request_data['vector'] #assigning it to vector
+        dataset = pd.read_csv('https://github.com/zsenani/medcoreML-2/blob/d9f5a98d70510682258f1e23bc3663c86a19455d/OO.csv?raw=true')
+        df = pd.DataFrame(dataset)
         #vector = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,0,0,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0]
         with open("pca.pkl", 'rb') as pca:
             pca = pk.load(pca)
